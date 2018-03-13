1d967e5b9794884fdc203dcdc12f43ed74963392
Alessandro Miliucci <lifeisfoo@gmail.com>
2018-03-13 16:51:03 +0100

    Build queue (#1)
    * first queue impl
    
    * fixed repo to checkout
    
    * fixed missing object
    
    * added all interesting events handling (no action)

---
+++ README.md
@@ -0,0 +1,78 @@
+
+## Manual install
+
+ubuntu 16.04:
+
+apt-get install redis-server
+systemctl status redis
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install nodejs
+git clone https://github.com/lifeisfoo/github-ci.git
+cd gituhub-ci
+npm install
+npm install pm2 -g
+vim .env
+pm2 start index.js
+ufw allow 3000
+pm2 logs
+pm2 reload all #dopo update
+
+install docker
+https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
+
+install compose
+https://docs.docker.com/compose/install/#prerequisites
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+
+https://help.github.com/articles/about-github-s-ip-addresses/
+https://api.github.com/meta
+
+## TODO
+
+ - [ ] WIP checkout the PR code using a queue and listen for response
+
+        // TODO: the build job will checkout
+        // and execute docker on a remote host/VM to build
+        // code is given from here (no github credential passing)
+
+// State be one of error, failure, pending, or success:
+//    marked has failed because the job failed to complete
+//    marked as error because the job did complete, but exited with a non-zero status
+//    marked as success because the job did complete, and exited with a zero status
+
+## Internals
+
+### Job queue
+
+The job queue is provided by Bull using Redis. A job object in the queue has this structure:
+
+```
+{
+  "id": "2",
+  "name": "__default__",
+  "data": {
+    "github": {
+      "access_token": "562e77axxx",
+      "full_repo_name": "Variazioni/test-ci",
+      "branch_name": "fix-from-FORK",
+      "commit_sha": "690c7c9f792915a84692be2c58e146adddf14b4b",
+      "main_repo_full_name": "lifeisfoo/test-ci"
+    },
+    "docker": {
+      "registry": "registry.hub.docker.com",
+      "user": "lifeisfoo",
+      "pass": "password",
+      "image_name": "lifeisfoo/test-ci",
+      "image_tag": "PR-2"
+    },
+    "out_dir": "/Users/alessandro/Dev/github-ci/builds/lifeisfoo/test-ci/PR-2-Variazioni_test-ci:fix-from-FORK:690c7c9f792915a84692be2c58e146adddf14b4b"
+  }
+}
+```
+
+### Repositories clone + checkout
+
+The PR branch is fully cloned (without --depth 1) because we need to checkout a specific commit.
+Why? Because the PR event can be delivered delayed or because the queue delay.
+In this case the branch HEAD could be different from the event commit sha.
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fse = require('fs-extra');
 const github = require('octonode');
-const commands = require('./lib/commands');
+const Queue = require('bull');
+
+require('dotenv').config();
 
 const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 
@@ -12,115 +14,151 @@ const DOCKER_REGISTRY = process.env.DOCKER_REGISTRY || 'registry.hub.docker.com'
 
 const BUILDS_DIR = process.env.BUILDS_DIR || './builds';
 
+const ACTIVE_REPOS = process.env.ACTIVE_REPOS.split(',');
+const REPOS_IMAGES_MAP = process.env.REPOS_IMAGES_MAP.split(',').reduce((acc, cur) => {
+    const repo_image = cur.split(' ');
+    acc[repo_image[0]] = repo_image[1];
+    return acc;
+}, {});
 
+console.log('Active repositories are', JSON.stringify(ACTIVE_REPOS));
+console.log('Active repositories/images mapping are', JSON.stringify(REPOS_IMAGES_MAP));
 console.log('Using', BUILDS_DIR, 'as builds dir');
 
 fse.ensureDir(BUILDS_DIR)
     .then(() => {
-        console.log('success!')
+        console.log('Builds dir created!')
     })
     .catch(err => {
         console.error(err)
     });
 
-const client = github.client(GITHUB_ACCESS_TOKEN);
-const ghrepo = client.repo('lifeisfoo/test-ci');
-const ghpr = client.pr('lifeisfoo/test-ci', 1);
+const client = github.client(GITHUB_ACCESS_TOKEN);//ACCESS as a user
+
+const builds_queue = new Queue('builds', {redis: {port: 6379, host: '127.0.0.1'}});
+builds_queue.process(1, __dirname+'/lib/docker-processor.js');
+builds_queue.on('completed', function(job, result){
+    // Job completed with output result!
+    console.log('Completed job', JSON.stringify(job), 'with result', JSON.stringify(result));
+    const ghrepo = client.repo(job.data.github.base_repo_full_name);
+    ghrepo.status(job.data.github.commit_sha, {
+      "state": "success",
+      "target_url": "http://ci.mycompany.com/job/hub/3",
+      "description": "Build completed, merge!"
+    },(err, data, headers) => {console.log('PR state set to success');});
+});
+
+builds_queue.on('failed', function(job, err){
+    // A job failed with reason `err`!
+    console.log('Job failed, err:', JSON.stringify(err), 'Job:', JSON.stringify(job));
+    const ghrepo = client.repo(job.data.github.base_repo_full_name);
+    if (err.is_build_error) {
+        ghrepo.status(job.data.github.commit_sha, {
+          "state": "error",
+          "target_url": "http://ci.mycompany.com/job/hub/3",
+          "description": "Build error, fix it and retry!"
+        },(err, data, headers) => {console.log('PR state set to error');});
+    } else {
+        ghrepo.status(job.data.github.commit_sha, {
+          "state": "failed",
+          "target_url": "http://ci.mycompany.com/job/hub/3",
+          "description": "Build failed, a CI internal error status!"
+        },(err, data, headers) => {console.log('PR state set to failed');});
+    }
+});
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/events-handler', (req, res) => {
-    if (req.headers['x-github-event'] === 'pull_request') {
+    if (req.headers['x-github-event'] === 'push') {
+        const push = req.body;
+        const last_commit_sha = push.head_commit.id;
+        const repo_full_name = push.repository.full_name;
+        const mapped_image_name = REPOS_IMAGES_MAP[repo_full_name] ?
+            REPOS_IMAGES_MAP[repo_full_name] : base_repo_full_name; // by convention
+
+        console.log('Info about a Push:');
+        console.log('Repo is ', repo_full_name);
+        console.log('Latest commit is ', last_commit_sha);
+        console.log('Image name is', mapped_image_name);
+
+        console.log('Skipping build, not implemented yet');
+        res.status(200).end();
+
+    } else if (req.headers['x-github-event'] === 'release') {
+        const release = req.body;
+        const tag_released = release.release.tag_name;
+        const repo_full_name = release.repository.full_name;
+        const mapped_image_name = REPOS_IMAGES_MAP[repo_full_name] ?
+            REPOS_IMAGES_MAP[repo_full_name] : base_repo_full_name; // by convention
+
+        console.log('Info about a Release:');
+        console.log('Repo is ', repo_full_name);
+        console.log('Tag released is ', tag_released);
+        console.log('Image name is', mapped_image_name);
+
+        console.log('Skipping build, not implemented yet');
+        res.status(200).end();
+
+    } else if (req.headers['x-github-event'] === 'pull_request') {
+        // use this only for repo active check
+        const repo = req.body.repository.full_name;
+
+        if (ACTIVE_REPOS.indexOf(repo) === -1) {
+            console.log(repo, 'not active for CI');
+            return res.status(200).end();
+        }
+
         console.log('Info about a Pull Request:');
         const pr = req.body.pull_request;
         const last_commit_sha = pr.head.sha;
-        const main_repo_full_name = pr.base.repo.full_name;
+        const base_repo_full_name = pr.base.repo.full_name;
         const source_repo_full_name = pr.head.repo.full_name;
         const source_repo_branch_name = pr.head.ref;
         const pr_number = pr.number;
-        console.log('Main repo is ', main_repo_full_name);
+        const mapped_image_name = REPOS_IMAGES_MAP[base_repo_full_name] ?
+            REPOS_IMAGES_MAP[base_repo_full_name] : base_repo_full_name; // by convention
+
+        console.log('Main repo is ', base_repo_full_name);
         console.log('Source repo is ', source_repo_full_name);
         console.log('Source branch is ', source_repo_branch_name);
         console.log('Latest commit is ', last_commit_sha);
         console.log('PR number is ', pr_number);
+        console.log('Image name is', mapped_image_name);
 
-        // TODO: checkout the PR code using a queue and listen for response
-
-        // CLONE + CHECKOUT
-        // The clone is of the full branch (without --depth 1) because we need
-        // to checkout a specific commit. Why? Because the PR event can be delivered delayed
-        // or because the queue delay. In this case the branch HEAD could be different from
-        // the event commit sha
-
-        // TODO: the build job will checkout
-        // and execute docker on a remote host/VM to build
-        // code is given from here (no github credential passing)
-
-// State be one of error, failure, pending, or success:
-//    marked has failed because the job failed to complete
-//    marked as error because the job did complete, but exited with a non-zero status
-//    marked as success because the job did complete, and exited with a zero status
-
-        const out_dir = `${__dirname}/builds/${main_repo_full_name}:${source_repo_full_name}:${source_repo_branch_name}:${last_commit_sha}`;
+        const out_dir = `${__dirname}/builds/${base_repo_full_name}:${source_repo_full_name}:${source_repo_branch_name}:${last_commit_sha}`;
         const builds_dir = `${__dirname}/builds`;
         const pr_commit_dir = `PR-${pr_number}-${source_repo_full_name}:${source_repo_branch_name}:${last_commit_sha}`.replace('/', '_');
-        const pr_checkout_dir = `${builds_dir}/${main_repo_full_name}/${pr_commit_dir}`;
+        const pr_checkout_dir = `${builds_dir}/${base_repo_full_name}/${pr_commit_dir}`;
+
+        const github_conf = {
+            access_token: GITHUB_ACCESS_TOKEN,
+            full_repo_name: source_repo_full_name,
+            branch_name: source_repo_branch_name,
+            commit_sha: last_commit_sha,
+            base_repo_full_name: base_repo_full_name
+        };
+        const docker_conf = {
+            registry: DOCKER_REGISTRY,
+            user: DOCKER_USER,
+            pass: DOCKER_PASS,
+            image_name: mapped_image_name,
+            image_tag: `PR-${pr_number}`
+        };
+        
+        const ghrepo = client.repo(base_repo_full_name);
         ghrepo.status(last_commit_sha, {
           "state": "pending",
           "target_url": "http://ci.mycompany.com/job/hub/3",
           "description": "Build is running, please be patient."
         }, (err, data, headers) => {
-            //console.log('PR status set to pending');
-            const github_conf = {
-                access_token: GITHUB_ACCESS_TOKEN,
-                full_repo_name: source_repo_full_name,
-                branch_name: source_repo_branch_name,
-                commit_sha: last_commit_sha
-            };
-            const docker_conf = {
-                registry: DOCKER_REGISTRY,
-                user: DOCKER_USER,
-                pass: DOCKER_PASS,
-                image_name: main_repo_full_name, // by convention
-                image_tag: `PR-${pr_number}`
-            };
-
-            console.log('Cloning the repository...');
-            const clone_build_test_cmd = commands.clone_build_test(github_conf, docker_conf, pr_checkout_dir); 
-                /*GITHUB_ACCESS_TOKEN,
-                source_repo_full_name,
-                source_repo_branch_name,
-                last_commit_sha,
-                pr_checkout_dir);*/
-            clone_build_test_cmd.on('close', (exit_code) => {
-                console.log(`git clone, build and test exited with code ${exit_code}`);
-                if (exit_code === 0) {
-                    ghrepo.status(last_commit_sha, {
-                      "state": "success",
-                      "target_url": "http://ci.mycompany.com/job/hub/3",
-                      "description": "Build completed, merge!"
-                    },(err, data, headers) => {console.log('PR state set to success');});
-                } else {
-                    ghrepo.status(last_commit_sha, {
-                      "state": "error",
-                      "target_url": "http://ci.mycompany.com/job/hub/3",
-                      "description": "Build error, fix it and retry!"
-                    },(err, data, headers) => {console.log('PR state set to error');});
-                }
+            builds_queue.add({
+                github: github_conf,
+                docker: docker_conf,
+                out_dir: pr_checkout_dir
             });
-            clone_build_test_cmd.on('error', (err) => {
-                console.log(err);
-                ghrepo.status(last_commit_sha, {
-                      "state": "failed",
-                      "target_url": "http://ci.mycompany.com/job/hub/3",
-                      "description": "Build failed, a CI internal error status!"
-                    },(err, data, headers) => {console.log('PR state set to failed');});
-            })
-            //setTimeout(() => {
-                
-            //}, 10000)
-        }); // created status
+        });
 
     } else if (req.headers['x-github-event'] === 'status') {
         console.log('Received a status:');
@@ -129,20 +167,15 @@ app.post('/events-handler', (req, res) => {
         console.log('Description is ', req.body.description);
         console.log('Commit is ', req.body.sha);
         console.log('Target url is ', req.body.target_url);
+        res.status(200).end();
     } else {
         console.log(req.headers);
         console.log(req.query);
         console.log(req.params);
         console.log(req.body);
+        res.status(200).end();
     }
     console.log('\n\n\n');
-    //ghpr.info((err, data, headers) => {
-    //    console.log('PR GET NOW\n');
-    //      console.log("error: " + err);
-    //      console.log("data: " + JSON.stringify(data));
-    //      console.log("headers:" + JSON.stringify(headers));
-    //    console.log('\n\n\n');
-    //});
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
