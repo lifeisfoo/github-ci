c07a3e76850ea5ec8dd8bc738136d18e43b686b5
Alessandro Miliucci <lifeisfoo@gmail.com>
2018-03-14 16:21:24 +0100

    fixed latest push

--- lib/clone_build_test.sh
+++ lib/clone_build_test.sh
@@ -11,4 +11,4 @@ git clone -b $BRANCH \
         --abort-on-container-exit \
         --exit-code-from sut && \
     docker push $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_TAG && \
-    if [ "$IMAGE_TAG" = "master" ]; then docker push $DOCKER_REGISTRY/$IMAGE_NAME:latest; fi
+    if [ "$IMAGE_TAG" = "master" ]; then docker tag $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_TAG $DOCKER_REGISTRY/$IMAGE_NAME:latest && docker push $DOCKER_REGISTRY/$IMAGE_NAME:latest; fi
